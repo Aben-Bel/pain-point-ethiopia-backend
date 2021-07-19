b6ea3c9be8fbc549e568d1544385bf5c8425a3f1
aben-bel <abenezer.sbelay@gmail.com>
2021-07-19 11:01:55 +0300

    setup starter project

---
+++ painpoint/src/test/java/com/painpoint/PainpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.painpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
